56ec702ea593e9bc528adc7e1ac8aeb03a2feb0d
Michael Roth <michael.franklin.roth@gmail.com>
2019-08-24 22:17:03 -0400

    grid creation as an object instead of an array works

--- matrix.js
+++ matrix.js
@@ -1,36 +1,17 @@
 class Grid {
-  constructor() {
-    this.message = "test"
-    this.rowLength = 5;
-    this.colLength = 5;
-
-  }
   createGrid() {
-    // this.gridContainer = {};
-    // for(let x = 1; x <= this.rowLength; x++) {
-    //   for(let y = 1; y <= this.colLength; y++) {
-    //     this.gridContainer[`r${x}c${y}`] = "_"
-    //   }
-    // }
-    this.gridContainer = [];
-    let cols = []
-    for (let i = 0; i < this.colLength; i++) {
-
+    for (let i = 1; i <= 3; i++) {
+      let tempColObj = {};
+      for (let j = 1; j <= 3; j++) {
+        tempColObj[`col${j}`] = '_'
+      }
+      this[`row${i}`] = tempColObj
     }
   }
 }
 
-// myGrid = new Grid;
-// myGrid.createGrid();
-// myGrid.gridContainer['r1c1']="x"
 
-// console.table(myGrid.gridContainer)
-
-let myObj1 = {}
-let myObj2 = {}
-let myObj3 = {}
-
-myObj1[`row1`] = { col1: [], col2: [] }
-myObj1[`row1`] = { col3: [], col4: [] }
-
-console.table(myObj1)
+let myGrid = new Grid;
+myGrid.createGrid();
+myGrid.row1.col2 = 'x'
+console.table(myGrid)
