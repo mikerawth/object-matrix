f6cce480d09a4564bf3ea814b7627153750cb286
Michael Roth <michael.franklin.roth@gmail.com>
2019-08-24 22:41:25 -0400

    final touches.  Now can create Grid with desired row and col length, and the constructor of Grid will call upon it's own function

--- matrix.js
+++ matrix.js
@@ -1,9 +1,12 @@
 class Grid {
-  createGrid() {
-    for (let i = 1; i <= 3; i++) {
+  constructor(theRowLength, theColLength) {
+    this.createGrid(theRowLength, theColLength);
+  }
+  createGrid(rowLength, colLength) {
+    for (let i = 1; i <= rowLength; i++) {
       let tempColObj = {};
-      for (let j = 1; j <= 3; j++) {
-        tempColObj[`col${j}`] = '_'
+      for (let j = 1; j <= colLength; j++) {
+        tempColObj[`col${j}`] = "";
       }
       this[`row${i}`] = tempColObj
     }
@@ -11,7 +14,7 @@ class Grid {
 }
 
 
-let myGrid = new Grid;
-myGrid.createGrid();
-myGrid.row1.col2 = 'x'
+let myGrid = new Grid(5, 5);
+myGrid.row2.col2 = "x"
+myGrid.row3.col5 = "x"
 console.table(myGrid)
